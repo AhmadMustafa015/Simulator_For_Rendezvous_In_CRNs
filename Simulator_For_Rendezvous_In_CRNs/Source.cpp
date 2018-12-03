b90c8e77946a72aef720bc189a6a240bbe05e6d1
AhmadMustafa015 <ahm0157454@fet.ju.edu.jo>
2018-12-03 22:27:56 +0200

    changes

--- Simulator_For_Rendezvous_In_CRNs/Source.cpp
+++ Simulator_For_Rendezvous_In_CRNs/Source.cpp
@@ -2,7 +2,7 @@
 int main()
 {
 	//initializaion start(number of band , number of SUs , Probability of PU on) 
-	Initialization Start(100, 50, 0.2);
+	Initialization Start(100, 40 , .4);
 	Start.Initialize();
 	system("pause");
 }
