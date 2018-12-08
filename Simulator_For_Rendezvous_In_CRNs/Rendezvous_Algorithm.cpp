d791a9f562be40c45bbb2d5a8e166f22111b5c21
ahmad mustafa <ahmad-97311@hotmail.com>
2018-12-08 02:17:33 +0200

    special vector added

--- Simulator_For_Rendezvous_In_CRNs/Rendezvous_Algorithm.cpp
+++ Simulator_For_Rendezvous_In_CRNs/Rendezvous_Algorithm.cpp
@@ -370,22 +370,3 @@ void Rendezvous_Algorithm::removeFromSpecialBand(int B)
 	specialBands.erase(std::remove(specialBands.begin(), specialBands.end(), B), specialBands.end());
 	iterative.erase(std::remove(iterative.begin(), iterative.end(), B), iterative.end());
 }
-/*void insertionSort(int list[], int listLength)
-{
-int firstOutOfOrder, location;
-int temp;
-for (firstOutOfOrder = 1; firstOutOfOrder < listLength;
-firstOutOfOrder++)
-if (list[firstOutOfOrder] < list[firstOutOfOrder - 1])
-{
-temp = list[firstOutOfOrder];
-location = firstOutOfOrder;
-do
-{
-list[location] = list[location - 1];
-location--;
-}
-while (location > 0 && list[location - 1] > temp);
-list[location] = temp;
-}
-} //end insertionSort*/
